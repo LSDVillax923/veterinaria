663499b4ef0fecd004992285b24d046736cc2bc2
Carol Rios <rios_carolf@javeriana.edu.co>
2026-02-26 11:38:58 -0500

    cambios

--- demo/src/main/java/com/example/demo/repository/MascotaRepository.java
+++ demo/src/main/java/com/example/demo/repository/MascotaRepository.java
@@ -15,8 +15,6 @@ public class MascotaRepository {
     
     private Map<Integer, Mascota> data = new HashMap<>();
 
-
-    
     public MascotaRepository() {
         data.put(1, new Mascota(1, "Firulais", "Perro","Labrador", 5, 10.5, "foto1.jpg", "Sano", "Sin enfermedad", "Muy juguetón"));
         data.put(2, new Mascota(2, "Michi", "Gato", "Siames", 3, 4.2, "foto2.jpg", "Sano", "Sin enfermedad", "Muy cariñoso"));
